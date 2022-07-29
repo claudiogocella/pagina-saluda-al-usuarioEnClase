e6e8e44d0e8e7b77c65cfe611aa987f7ce2f80c4
claudiogocella <104107475+claudiogocella@users.noreply.github.com>
2022-07-29 18:21:25 -0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+//console.log("Hola usuario");
+
+//alert ("Hola usuario");
+
+let boton = document.getElementById("saludar");
+
+function saludar() {
+  alert("Hola!");
+}
+
+boton.addEventListener("click", saludar);
